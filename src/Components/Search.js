d7b119abf36edc37f0b08b9d51eb68be7594fc38
filipvlad2 <105279905+filipvlad2@users.noreply.github.com>
2022-08-11 14:11:11 +0300

    Update Search.js

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,7 +1,12 @@
+import {useRef} from 'react';
+
 function Search(props) {
 
+	const ref = useRef(null);
+
 	function handleSearchClick() {
-		let searchedWord = document.getElementById("searchInput").value;
+		let searchedWord = ref.current.value;
+
 		let foundWord = props.searchWordsList.find((word) => word === searchedWord);
 
 		if (foundWord !== searchedWord) {
@@ -14,8 +19,8 @@ function Search(props) {
 	return (
 		<div className = "searchDiv">
 			<label>Search the word</label>
-			<input type = "text" id = "searchInput"></input>
-			<button onClick={() => handleSearchClick()}>Search</button>
+			<input type = "text" id = "searchInput" ref = {ref}></input>
+			<button onClick = {() => handleSearchClick()}>Search</button>
 		</div>
 	)
 }
